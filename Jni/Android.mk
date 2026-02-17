52acc8659781638c51752a93608367c964485667
disco sys <ceanjhadengcatral@gmail.com>
2026-02-17 19:36:43 +0800

    Update Android.mk

--- Jni/Android.mk
+++ Jni/Android.mk
@@ -1,1 +1,8 @@
 
+LOCAL_PATH := $(call my-dir)
+include $(CLEAR_VARS)
+LOCAL_MODULE    := vulkan.mt6768
+LOCAL_SRC_FILES := ../src/main.cpp
+LOCAL_LDLIBS    := -llog -ldl
+LOCAL_CPPFLAGS  := -std=c++17
+include $(BUILD_SHARED_LIBRARY)
